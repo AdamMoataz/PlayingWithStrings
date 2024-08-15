306486d691bf2f90c27981867cde6b65f95b1c66
Adam Moataz <adam.moataz.hindawi2014@gmail.com>
2024-08-15 21:16:20 +0300

    playing with strings project

---
+++ main.py
@@ -0,0 +1,17 @@
+# I will collect personal informtion
+
+#i will collect his name 
+name=input("please enter your name ")
+print(f"hello {name}")
+
+# i well collect the age
+age= int(input("please enter your age "))
+print()
+# collecting the hobby
+hobbies=input("please enter your hobbies ")
+
+# printing the details
+print("your details are as follow ")
+print(f"name: {name}")
+print(f"age: {age}")
+print(f"hobbies {hobbies}")
